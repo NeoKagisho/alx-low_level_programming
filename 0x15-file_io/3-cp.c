8c60379204753817cc1061b9ebbefbd95e446535
NeoKagisho <144424944+NeoKagisho@users.noreply.github.com>
2023-12-01 14:34:54 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,11 +8,10 @@ void close_file(int fd);
 /**
  * creates_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
+ *
  * Return: A pointer to the newly-allocated buffer.
  */
-
 char *creates_buffer(char *file)
-
 {
 char *buffer;
 
@@ -32,9 +31,7 @@ return (buffer);
  * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
  */
-
 void close_file(int fd)
-
 {
 int c;
 
@@ -51,15 +48,15 @@ exit(100);
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
+ *
  * Return: 0 on success.
+ *
  * Description: If the argument count is incorrect - exit code 97.
  * If file_from does not exist or cannot be read - exit code 98.
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
  */
-
 int main(int argc, char *argv[])
-
 {
 int from, to, r, w;
 char *buffer;
